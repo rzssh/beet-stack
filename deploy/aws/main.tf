0e71bf5bda301c5bd34d056dd18de616a75c1ed6
rzssh <reactionnnnn@gmail.com>
2026-08-13 01:24:22 +0200

    ecs: grant task exec pull and logs

--- deploy/aws/main.tf
+++ deploy/aws/main.tf
@@ -73,9 +73,9 @@ resource "aws_iam_role" "task_exec" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow"
+      Effect    = "Allow"
       Principal = { Service = "ecs-tasks.amazonaws.com" }
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
 }
@@ -94,6 +94,11 @@ resource "aws_iam_role_policy" "task_exec_secrets" {
       ]
     }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "task_exec_managed" {
+  role       = aws_iam_role.task_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_ecs_task_definition" "beet" {
